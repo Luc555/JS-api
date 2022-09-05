282b4f8e67b4b6013e7dab9a004d3b1e0067dcd4
Lucas Martins <LUCAS.MARTINS.CMP@albacete.matriz>
2022-09-05 06:50:17 -0300

    Retorno de função

--- index.js
+++ index.js
@@ -20,24 +20,31 @@ let url = `http://api.openweathermap.org/data/2.5/weather?q=${cidade}&${limit}&a
 
 
 let dados ='';
- 
-request(url, function (err, response, body) {
-if(err){
-    console.log('error:', error);   
-} else {
-let weather = JSON.parse(body);
-dados = `Dados Metereológicos para ${weather.name}, ${weather.state}, ${weather.sys.country}:
--vento:  ${weather.wind.speed}~^C
--Temp Máxima: ${weather.main.temp_max}ºC
--Temp Minima: ${weather.main.temp_min}ºC
--Humidade: ${weather.main.humidity}%
--Nível do mar: ${weather.main.sea_level}m`;console.log(dados)
-let temp = `${weather.main.temp}`-273.15;
-let tempMAX = `${weather.main.temp_max}`-273.15;
+function showWeather(){
+    request(url, function (err, response, body) {
+        if(err){
+            console.log('error:', error);   
+    }   
+        else {
 
-console.log('-Temperatura: '+temp+'ºC');
-console.log('-Temperatura máxima: '+tempMAX+'ºC')
-console.log(typeof(temp));
-//console.log(weather);
-}
+            let weather = JSON.parse(body);
+            dados = `Dados Metereológicos para ${weather.name}, ${weather.state}, ${weather.sys.country}:
+            -vento:  ${weather.wind.speed}
+            -Temp Máxima: ${weather.main.temp_max}ºC
+            -Temp Minima: ${weather.main.temp_min}ºC
+            -Humidade: ${weather.main.humidity}%
+            -Nível do mar: ${weather.main.sea_level}m`;console.log(dados)
+            
+            console.log(typeof(temp));
+        //console.log(weather);
+            
+            
+        
+    }  
+        return dados;
+    
 });
+        
+}       
+        
+        console.log(showWeather());
