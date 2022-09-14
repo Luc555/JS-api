751127f2adffe495d7044da962d120c14d752eca
Lucas Martins <LUCAS.MARTINS.CMP@albacete.matriz>
2022-09-14 13:42:51 -0300

    Integrando API com frontend

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const cors = require('cors');
 const express = require('express')
 const app = express()
 
+app.use(cors());
 
 app.get('/', (req, res) => {
-    return res.json({message: 'OK'})
+    return res.json({message: 'Batata'})
 })
 
 app.listen('4567')
